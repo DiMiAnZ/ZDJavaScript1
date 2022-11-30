3991935b100572ec164a840b2a9ed7fa3fd60008
DiMiAnZ <zverevdmitriya@gmail.com>
2022-11-30 14:33:09 +0100

    HW 6 new

--- ZD HW 6/main.js
+++ ZD HW 6/main.js
@@ -114,11 +114,50 @@
 //===========================================================================================================
 //
 // описати колоду карт
+
+let mastsandcolors = [
+    {mast: 'heart', color: 'red'},
+    {mast: 'diamond', color: 'red'},
+    {mast: 'spade', color: 'black'},
+    {mast: 'clubs', color: 'black'},
+]
+let cardNames = ['6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+let koloda = [];
+
+for (const mastandcolor of mastsandcolors) {
+    for (const cardName of cardNames) {
+        let card = {
+            mast: mastandcolor.mast,
+            valueCard: cardName,
+            color: mastandcolor.color
+        }
+        koloda.push(card);
+    }
+}
+koloda.push({mast: 'joker', valueCard: null, color: 'red'});
+koloda.push({mast: 'joker', valueCard: null, color: 'black'});
+//console.log(koloda);
 // - знайти піковий туз
+
+//let filter = koloda.filter(tuz => tuz.mast === 'spade' && tuz.valueCard === 'ace');
+//console.log(filter);
+
 // - всі шістки
+
+//let filter = koloda.filter(sest => sest.valueCard === '6');
+//console.log(filter);
 // - всі червоні карти
+
+//let filter = koloda.filter(cvet => cvet.color === 'red');
+//console.log(filter);
 // - всі буби
+
+//let filter = koloda.filter(bubna => bubna.mast === 'diamond');
+//console.log(filter);
 // - всі трефи від 9 та більше
+
+//let filter = koloda.filter(trefa => trefa.mast === 'clubs' && trefa.valueCard >= '9' || trefa.mast === 'clubs' && trefa.valueCard === '10');
+//console.log(filter);
 //
 
 // {
@@ -131,6 +170,25 @@
 //
 // Додатково по reduce
 // Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
+
+let reduce = koloda.reduce((accumulator, value) => {
+    if (value.mast === 'spade') {
+        accumulator.spades.push(value);
+    } else if (value.mast === 'diamond') {
+        accumulator.diamonds.push(value);
+    } else if (value.mast === 'heart') {
+        accumulator.hearts.push(value);
+    } else if (value.mast === 'clubs') {
+        accumulator.clubses.push(value)
+    }
+    return accumulator;
+}, {
+    spades:[],
+    diamonds:[],
+    hearts:[],
+    clubses:[]
+});
+console.log(reduce);
 // {
 //     spades:[],
 //         diamonds:[],
